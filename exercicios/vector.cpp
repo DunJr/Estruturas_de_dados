4ea2a3f6f7a1d635325844938aaac9f4b0b3e9a1
carlos <carlosroberiojp@hotmail.com>
2022-09-08 17:29:38 -0300

    update

---
+++ exercicios/vector.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    constexpr size_t N = 1000;
+    std::vector<int> vec;
+    vec.resize(N, 0);
+    vec.resize(N, 0);
+
+
+
+
+    return 0;
+}
